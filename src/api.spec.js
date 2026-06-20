ff167ad4531381bcb59f0153710c0b290d0642a5
swonga <111727042+swonga@users.noreply.github.com>
2026-06-20 21:28:29 +1000

    reverted back to 10

--- src/api.spec.js
+++ src/api.spec.js
@@ -64,7 +64,7 @@ describe("API Contract Test", () => {
             .withPredicate(
               new EqualPredicate()
                 .withMethod(HttpMethod.GET)
-                .withPath("/product/11")
+                .withPath("/product/10")
             )
             .withResponse(
               new Response().withStatusCode(200).withJSONBody(expectedProduct)
